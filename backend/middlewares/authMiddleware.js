788c42adce54f1900908c0803df631c37d53438b
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-09-30 12:32:45 +0530

    amazon

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -21,6 +21,7 @@ const protect = asyncHandler(async (req, res, next) => {
         }
     }
 
+    
     if (!token) {
         res.status(401);
         throw new Error('Not authorized, no token');
