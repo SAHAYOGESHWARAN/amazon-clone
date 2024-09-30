447b56c8fdb50ada0882c2bf7b25924478036512
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-09-30 11:27:43 +0530

    amazon

--- frontend/store.js
+++ frontend/store.js
@@ -1,12 +1,22 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import { userLoginReducer, userRegisterReducer } from './reducers/userReducers';
+import { productListReducer } from './reducers/productReducers';  // assuming product reducers exist
 
 const reducer = combineReducers({
-    // Your reducers here
+    productList: productListReducer,
+    userLogin: userLoginReducer,
+    userRegister: userRegisterReducer,
 });
 
-const initialState = {};
+const userInfoFromStorage = localStorage.getItem('userInfo')
+    ? JSON.parse(localStorage.getItem('userInfo'))
+    : null;
+
+const initialState = {
+    userLogin: { userInfo: userInfoFromStorage },
+};
 
 const middleware = [thunk];
 
