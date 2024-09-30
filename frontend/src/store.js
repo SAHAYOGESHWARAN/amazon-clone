27a7ce93ae2ec5198b12528c673e398426279ce6
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-09-30 13:32:26 +0530

    amazon

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -3,32 +3,38 @@ import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { productListReducer } from './reducers/productReducers';
 import { userLoginReducer, userRegisterReducer } from './reducers/userReducers';
-import { orderCreateReducer } from './reducers/orderReducers'; // Order reducer
+import { orderCreateReducer } from './reducers/orderReducers';
 import { categoryListReducer } from './reducers/categoryReducers';
 import { wishlistReducer } from './reducers/wishlistReducers';
-const reducer = combineReducers({
+
+// Combine all the reducers into a root reducer
+const rootReducer = combineReducers({
     productList: productListReducer,
     userLogin: userLoginReducer,
     userRegister: userRegisterReducer,
-    orderCreate: orderCreateReducer, 
+    orderCreate: orderCreateReducer,
     categoryList: categoryListReducer,
-    wishlist: wishlistReducer, // Include order reducer
+    wishlist: wishlistReducer,
 });
 
+// Load user info from local storage if it exists
 const userInfoFromStorage = localStorage.getItem('userInfo')
     ? JSON.parse(localStorage.getItem('userInfo'))
     : null;
 
+// Initial state for the store
 const initialState = {
     userLogin: { userInfo: userInfoFromStorage },
 };
 
+// Middleware to be used
 const middleware = [thunk];
 
+// Create the Redux store with middleware and DevTools support
 const store = createStore(
-    reducer,
-    initialState,
-    composeWithDevTools(applyMiddleware(...middleware))
+    rootReducer, // Combined reducer
+    initialState, // Initial state
+    composeWithDevTools(applyMiddleware(...middleware)) // Apply middleware and DevTools
 );
 
 export default store;
